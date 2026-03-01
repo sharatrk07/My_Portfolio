61d2ad9f9e677839b4599adf87ad8258c6712b3b
sharatrk07 <121000315+sharatrk07@users.noreply.github.com>
2026-03-01 18:46:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const canvas = document.getElementById('particleCanvas');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 let particlesArray = [];
 
 class Particle {
